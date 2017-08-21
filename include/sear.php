d4dbebd16c876ed50a18a5f618b6e9b8fb749821
t4cgbb <t4cgbb@gmail.com>
2017-08-21 16:13:25 +0800

    搜尋欄

---
+++ include/sear.php
@@ -0,0 +1,15 @@
+
+                <!-- Start of Search Wrapper -->
+                <div class="search-area-wrapper">
+                        <div class="search-area container">
+                                <h3 class="search-header">您可以查詢文章關鍵字</h3>
+                                
+
+                                 <form class="search-form clearfix">
+                                        <a href="<?php echo $mylink ?>/articles-list.php"><input class="search-btn" type="button" value="搜尋" onclick="startSearch()"/></a>
+                                        <div id="search-error-container"></div>
+                                </form>
+                                
+                        </div>
+                </div>
+                <!-- End of Search Wrapper -->
